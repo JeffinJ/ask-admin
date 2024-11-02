36a6f7bb3ceb7656b54eabad5365b8499ed067e4
Jeffin <jeffinj787@gmail.com>
2024-11-02 16:48:50 +0000

    feat(questions-page): add questions page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { QuestionsTable } from "@/components/questions-table";
-import { getQuestions } from "@/server/apis/question";
-import { Suspense } from "react";
+import Link from "next/link";
 
 export default async function Home() {
-  const data = await getQuestions();
-
   return (
     <div className="text-white h-screen flex flex-col">
 
-      <div className="w-full p-40">
-        <Suspense fallback={<div>Loading...</div>}>
-          <QuestionsTable questions={data} />
-        </Suspense>
+      <div className="w-full p-40 flex flex-col space-y-5">
+        <div className="font-bold">
+          HOME DASHBOARD
+        </div>
+
+        <div>
+          <Link href="/questions">Questions</Link>
+        </div>
+
       </div>
     </div>
   );
